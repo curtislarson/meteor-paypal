e1ff92786653623adbb9a2ff303b3f05dcb40389
Curtis Larson <curtismlarson@gmail.com>
2015-04-03 14:39:32 -0400

    Added client and server paypal files

---
+++ client/paypal.js
@@ -0,0 +1,84 @@
+(function () {
+
+/* Imports */
+var Meteor = Package.meteor.Meteor;
+var OAuth = Package.oauth.OAuth;
+var Oauth = Package.oauth.Oauth;
+var _ = Package.underscore._;
+var ServiceConfiguration = Package['service-configuration'].ServiceConfiguration;
+var Random = Package.random.Random;
+
+/* Package-scope variables */
+var PayPal;
+
+(function () {
+
+PayPal = {};
+
+// Request PayPal credentials for the user
+// @param options {optional}
+// @param credentialRequestCompleteCallback {Function} Callback function to call on
+//   completion. Takes one argument, credentialToken on success, or Error on
+//   error.
+PayPal.requestCredential = function (options, credentialRequestCompleteCallback) {
+  // support both (options, callback) and (callback).
+  if (!credentialRequestCompleteCallback && typeof options === 'function') {
+    credentialRequestCompleteCallback = options;
+    options = {};
+  } else if (!options) {
+    options = {};
+  }
+
+  var config = ServiceConfiguration.configurations.findOne({service: 'paypal'});
+  if (!config) {
+    credentialRequestCompleteCallback && credentialRequestCompleteCallback(
+      new ServiceConfiguration.ConfigError());
+    return;
+  }
+
+  var credentialToken = Random.secret();
+
+  var scope = ['client_credentials'];
+  if (options.requestPermissions)
+    scope = options.requestPermissions;
+  scope = _.union(scope, requiredScope);
+  var flatScope = _.map(scope, encodeURIComponent).join('+');
+
+  // https://developers.google.com/accounts/docs/OAuth2WebServer#formingtheurl
+  var accessType = options.requestOfflineToken ? 'offline' : 'online';
+  var approvalPrompt = options.forceApprovalPrompt ? 'force' : 'auto';
+
+  var loginStyle = OAuth._loginStyle('paypal', config, options);
+
+  var loginUrl =
+    config.url + '/v1/oauth2/token' +
+    '?response_type=code' +
+    '&client_id=' + config.clientId +
+    '&grant_type=' + flatScope +
+    '&redirect_uri=' + OAuth._redirectUri('paypal', config) +
+    '&state=' + OAuth._stateParam(loginStyle, credentialToken) +
+    '&access_type=' + accessType +
+    '&approval_prompt=' + approvalPrompt;
+
+  OAuth.launchLogin({
+    loginService: "paypal",
+    loginStyle: loginStyle,
+    loginUrl: loginUrl,
+    credentialRequestCompleteCallback: credentialRequestCompleteCallback,
+    credentialToken: credentialToken,
+    popupOptions: { height: 600 }
+  });
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+}).call(this);
+
+
+/* Exports */
+if (typeof Package === 'undefined') Package = {};
+Package.paypal = {
+  PayPal: PayPal
+};
+
+})();
---
+++ server/paypal.js
@@ -0,0 +1,96 @@
+(function () {
+
+/* Imports */
+var Meteor = Package.meteor.Meteor;
+var OAuth = Package.oauth.OAuth;
+var Oauth = Package.oauth.Oauth;
+var HTTP = Package.http.HTTP;
+var HTTPInternals = Package.http.HTTPInternals;
+var _ = Package.underscore._;
+var ServiceConfiguration = Package['service-configuration'].ServiceConfiguration;
+
+/* Package-scope variables */
+var PayPal;
+
+(function () {
+
+PayPal = {};
+
+OAuth.registerService('paypal', 2, null, function(query) {
+
+  var response = getTokens(query);
+  var accessToken = response.accessToken;
+  var idToken = response.idToken;
+
+  var serviceData = {
+    accessToken: accessToken,
+    idToken: idToken,
+    expiresAt: (+new Date) + (1000 * response.expiresIn)
+  };
+
+  var fields = identity;
+  _.extend(serviceData, fields);
+
+  // only set the token in serviceData if it's there. this ensures
+  // that we don't lose old ones (since we only get this on the first
+  // log in attempt)
+  if (response.refreshToken)
+    serviceData.refreshToken = response.refreshToken;
+
+  return {
+    serviceData: serviceData,
+  };
+});
+
+// returns an object containing:
+// - accessToken
+// - expiresIn: lifetime of token in seconds
+// - refreshToken, if this is the first authorization request
+var getTokens = function (query) {
+  var config = ServiceConfiguration.configurations.findOne({service: 'paypal'});
+  if (!config)
+    throw new ServiceConfiguration.ConfigError();
+
+  var response;
+  try {
+    response = HTTP.post(
+      config.url + "/v1/oauth2/token", {params: {
+        code: query.code,
+        client_id: config.clientId,
+        client_secret: OAuth.openSecret(config.secret),
+        redirect_uri: OAuth._redirectUri('paypal', config),
+        grant_type: 'client_credentials'
+      }});
+  } catch (err) {
+    throw _.extend(new Error("Failed to complete OAuth handshake with PayPal. " + err.message),
+                   {response: err.response});
+  }
+
+  if (response.data.error) { // if the http response was a json object with an error attribute
+    throw new Error("Failed to complete OAuth handshake with PayPal. " + response.data.error);
+  } else {
+    return {
+      accessToken: response.data.access_token,
+      refreshToken: response.data.refresh_token,
+      expiresIn: response.data.expires_in,
+      idToken: response.data.id_token
+    };
+  }
+};
+
+PayPal.retrieveCredential = function(credentialToken, credentialSecret) {
+  return OAuth.retrieveCredential(credentialToken, credentialSecret);
+};
+
+
+
+}).call(this);
+
+
+/* Exports */
+if (typeof Package === 'undefined') Package = {};
+Package.paypal = {
+  PayPal: PayPal
+};
+
+})();
